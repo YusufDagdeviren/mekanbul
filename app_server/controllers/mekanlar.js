114630fff9ba6ad4786723c7f255435c80e07b35
YusufDagdeviren <ddyusufdd@gmail.com>
2022-10-20 11:00:58 +0300

    Siteyi Dinam yaptim guncel

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -30,7 +30,51 @@ const anaSayfa=function(req, res, next) {
     );
 }
 const mekanBilgisi=function(req, res, next) {
-    res.render('mekanbilgisi', { title: 'Mekan Bilgisi' });
+    res.render('mekanbilgisi',
+    {  
+        "baslik":"Mekan Bilgisi",
+        "mekanBaslik":"Starbucks",
+        "mekanDetay":{
+            "ad":"Starbucks",
+            "puan":"3",
+            "adres":"Centrum Garden Avm",
+            "saatler":[
+                {
+                "gunler":"Pazartesi-Cuma",
+                "acilis":"9:00-23:00",
+                "kapali":"false"
+                },
+                {
+                    "gunler":"Cumartesi-Pazar",
+                    "acilis":"9:00-21:00",
+                    "kapali":"false"
+                }
+            ],
+            "imkanlar":[
+                "kahve","Çay","Kek"
+            ],
+            "koordinatlar":{
+                "enlem":"37.7",
+                "boylam":"30.5"
+            },
+            "yorumlar":[
+                {
+                    "yorumYapan":"Asım Sinan Yuksel",
+                    "yorumMetini":"berbaaaat",
+                    "tarih":"20 Ekim 2022",
+                    "puan":"1"
+                },
+                {
+                    "yorumYapan":"Zehra Çakır",
+                    "yorumMetini":"cok iyi",
+                    "tarih":"20 Kasım 2022",
+                    "puan":"5"
+                }
+            ]
+        }
+    }
+    
+    );
 }
 const yorumEkle=function(req, res, next) {
     res.render('yorumekle', { title: 'Yorum Ekle' });
