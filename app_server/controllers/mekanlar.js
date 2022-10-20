5faea3c5eec14268ad8ad21353accc24c7cbf6bd
YusufDagdeviren <ddyusufdd@gmail.com>
2022-10-20 10:02:52 +0300

    Siteyi Dinamik yaptıımm

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -2,7 +2,32 @@ var express = require('express');
 var router = express.Router();
 
 const anaSayfa=function(req, res, next) {
-    res.render('anasayfa', { title: 'Anasayfa' });
+    res.render('anasayfa',
+    {
+    "baslik":"Anasayfa",
+     "sayfabaslik":{
+        "siteAd":"MekanBul",
+        "Slogan":"Civardaki Mekanları keşfet!"
+     },   
+     "mekanlar":[
+        {
+            "ad":"Starbucks",
+            "puan":"3",
+            "adres":"Centrum Garden Avm",
+            "imkanlar":["Dunya Kahveleri","Cay","Kek"],
+            "mesafe":"10km"
+        },
+        {
+            "ad":"Barida Kafe",
+            "puan":"1",
+            "adres":"Sdu batı kampusu",
+            "imkanlar":["Tost","Cay","Kahve"],
+            "mesafe":"1km"
+        },
+        
+     ]
+    }
+    );
 }
 const mekanBilgisi=function(req, res, next) {
     res.render('mekanbilgisi', { title: 'Mekan Bilgisi' });
