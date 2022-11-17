72ad3dd1dd86efb4e1f615905a05c423a9f6beaa
YusufDagdeviren <ddyusufdd@gmail.com>
2022-11-17 21:06:09 +0300

    RestApi ile Guncellendi|odev5

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,9 +1,6 @@
 var mongoose=require("mongoose");
 require("./mekansema")//mekansemayi göstermemizi saglar
 //var dbURI = "mongodb://localhost/mekanbul";
-//var dbURI="mongodb+srv://yusuf:1234@mekanbul.1xvr5gn.mongodb.net/?retryWrites=true&w=majority";
-//var dbURI = "mongodb+srv://yusuf:1234@mekanbul.1xvr5gn.mongodb.net/test";
-//var dbURI = "mongodb+srv://yusuf:1234@mekanbul.xsxstub.mongodb.net/?retryWrites=true&w=majority"
 var dbURI = "mongodb+srv://yusuf:1234@mekanbul.lx5ado0.mongodb.net/?retryWrites=true&w=majority"
 mongoose.connect(dbURI);
 function kapat(msg,callback){
